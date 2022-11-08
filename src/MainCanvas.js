16ee248d6a4a939ad23f319eac948c4606085e8f
Jaren Kempton <48076699+JarenKempton@users.noreply.github.com>
2022-11-08 06:54:38 -0700

    added animation to clouds

--- src/MainCanvas.js
+++ src/MainCanvas.js
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import { ParallaxBanner, Parallax } from "react-scroll-parallax";
 import Mountains from "./img/placeholder-mtns.png";
 import Trees from "./img/placeholder-trees.webp";
 import Cloud from "./img/cloud.png"
 
 const MainCanvas = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(Math.floor(position));
+    };
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+    
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    console.log(scrollPosition);
+
     return (
         <div style={{ height: "200vh", backgroundColor: "red", marginTop: 300 }}>
 
-            <Parallax
+            {/* <Parallax
                 speed={-50}
                 translateX={["400px", "-700px"]} 
                 style={{ backgroundColor: "green", width: "fit-content", height: "fit-content" }}
@@ -21,7 +38,14 @@ const MainCanvas = () => {
                 style={{ backgroundColor: "orange", width: "fit-content", height: "fit-content", right: "100%" }}
             >
                 <img style={{ transform: "scale(1.5)" }} src={Cloud} />
-            </Parallax>
+            </Parallax> */}
+            <div style={{display: "flex", justifyContent: "center"}}>
+
+            <div style={{display: "flex", backgroundColor: "orange", height: 250, width: `${900 + (scrollPosition * 4.5)}px`, justifyContent: "space-between", alignItems: "flex-start" }}>
+                <img src={Cloud} style={{transform: "scale(1.5)", marginTop: 95, backgroundColor: "aquamarine"}} />
+                <img src={Cloud} style={{backgroundColor: "purple"}} />
+            </div>
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
